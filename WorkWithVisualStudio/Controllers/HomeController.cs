c76b92de385cd2cb831f984016f61ced9e591f6c
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-01 16:58:12 +0300

    Добавьте файлы проекта.

---
+++ WorkWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkWithVisualStudio.Models;
+
+namespace WorkWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(SimpleRepository.SharedRepository.Products);
+        }
+    }
+}
