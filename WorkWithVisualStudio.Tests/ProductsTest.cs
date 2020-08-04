2ed894b1d9ed72e730637c21594f5ae85b768b6f
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-04 17:37:23 +0300

    Write and usinf firsts tests

---
+++ WorkWithVisualStudio.Tests/ProductsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using WorkWithVisualStudio.Models;
+
+namespace WorkWithVisualStudio.Tests
+{
+    public class ProductsTest
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Name = "New NAme";
+            Assert.Equal("New NAme", p.Name);
+        }
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product { Name = "Test", Price = 100M };
+            p.Price = 200M;
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
