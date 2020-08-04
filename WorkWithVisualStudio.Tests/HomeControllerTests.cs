2605a5a564ec34fd2b973a5c86cbdd2ec7cfbb40
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-08-04 18:19:19 +0300

    Add new test

---
+++ WorkWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkWithVisualStudio.Controllers;
+using WorkWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelsComplete()
+        {
+            var controller = new HomeController();
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name &&
+            p1.Price == p2.Price));
+        }
+    }
+}
